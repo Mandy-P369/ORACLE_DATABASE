1f84a691407514999662c762aa5445ce78629a67
Mandy-P369 <aayushpateriya762@gmail.com>
2025-10-28 01:21:14 +0530

    Add files via upload

--- POSTGRESQL/ASSSIGNMENT_2/CREATION_OF_TABLE.sql
+++ POSTGRESQL/ASSSIGNMENT_2/CREATION_OF_TABLE.sql
@@ -11,6 +11,13 @@ CREATE TABLE University (
     location VARCHAR(255)
 );
 Select * from university;
+alter table university rename city to country  ;
+alter table university add column State varchar(50);
+alter table university rename location to States ;
+alter table university drop column states;
+alter table university add column city varchar(50);
+Select * from university;
+
 
 --College table 
 drop table colleges
@@ -21,6 +28,9 @@ CREATE TABLE Colleges (
     dean VARCHAR(255),
     established_year INT
  );
+
+ Select * from colleges;
+ 
 -- Department Table 
  CREATE TABLE Department (
     dept_id INT PRIMARY KEY,
@@ -28,6 +38,118 @@ CREATE TABLE Colleges (
     name VARCHAR(255) NOT NULL,
     building VARCHAR(255),
     head VARCHAR(255));
+
+Select * from department;
+
+--Course Table 
+CREATE TABLE Course (
+    course_id INT PRIMARY KEY,
+    dept_id INT NOT NULL references department,
+    name VARCHAR(255) NOT NULL,
+    credits INT,
+    description TEXT
+);
+Select * from Course;
+
+-- Instructor Table 
+CREATE TABLE Instructor (
+    instructor_id INT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    dept_id INT references department,
+    hire_date DATE,
+    title VARCHAR(50)
+);
+drop table instructor;
+Select * from Instructor;
+
+Select * from university;
+Select * from colleges;
+Select * from department;
+
+Select * from university;
+Select * from instructor;
+
+Select * from course;
+
+-- Section Table 
+CREATE TABLE Section (
+    section_id INT PRIMARY KEY,
+    course_id INT NOT NULL REFERENCES COURSE,
+    semester VARCHAR(20),
+    year INT,
+    instructor_id INT REFERENCES INSTRUCTOR,
+    classroom VARCHAR(50),
+    schedule VARCHAR(50)
+);
+Select * from section ;
+
+-- Student Table 
+CREATE TABLE Student ( student_id INT PRIMARY KEY, first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100) NOT NULL, dob DATE, gender VARCHAR(10), college_id INT NOT NULL references colleges,
+enrollment_year INT, major_dept_id INT references department );
+
+
+Select * from Student;
+
+-- Advisor Table 
+CREATE TABLE Advisor (
+    student_id INT NOT NULL  references Student,
+    instructor_id INT NOT NULL references instructor
+);
+Select * from Advisor;
+
+-- Enrollment Table 
+CREATE TABLE Enrollment (
+    student_id INT NOT NULL REFERENCES STUDENT,
+    section_id INT NOT NULL REFERENCES SECTION,
+    grade VARCHAR(2)
+);
+
+SELECT * FROM ENROLLMENT;
+
+-- Prerequisite Table 
+CREATE TABLE Prerequisite (
+    course_id INT NOT NULL references course,
+    prereq_course_id INT NOT NULL  references course
+);
+
+-- Query University
+Select * from university;
+-- Query Colleges 
+Select* from colleges ;
+-- Query Department
+Select * from department;
+-- Query Course
+Select * from course;
+-- Query instructor
+Select * from instructor;
+-- Query Section 
+Select * from section;
+-- Query Student
+Select * from student;
+-- Query Advisor 
+Select * from advisor;
+-- Query Enrollment
+Select * from enrollment;
+-- Query Prerequisite
+Select * from prerequisite;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
