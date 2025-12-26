d96b5c157d38e77101d5dcfc037ed341d3fc1abd
aayushmann pateriya <aayushpateriya762@gmail.com>
2025-12-26 22:25:22 +0530

    Add files via upload

--- ORACLE_AND_MONGODB/PRACTISE/TRIGGER/trig3.sql
+++ ORACLE_AND_MONGODB/PRACTISE/TRIGGER/trig3.sql
@@ -1,19 +1,23 @@
-create or replace trigger trig3 before insert on sales3 for each row 
+create or replace trigger trig3 before insert on sales2 for each row 
 declare 
 	v_count number ;
 begin
 	if :new.pname is null
 	then
-		select count(*) into v_count from sales3; 
+		select count(*) into v_count from sales2; 
 		:new.pname:='Product'||(v_count+1);
-	elsif :new.ppunit is null 
+	elsif :new.ppunit is null
 	then 
 		:new.ppunit:=0;
 	elsif :new.quantity is null
 	then
 		:new.quantity:=0;
-	
+	elsif :new.discount is null or :new.discount=0
+	then
+		:new.finalamount := :new.iamt+(:new.iamt*18/100);
 	end if;
-	:new.initialamount := :new.ppunit*:new.quantity;	
+	:new.iamt := :new.ppunit*:new.quantity;
+	:new.finalamount:= (:new.iamt-(:new.iamt*:new.discount/100))+(:new.iamt-(:new.iamt*:new.discount/100))*18/100;
+
 end;
 /
